dbd884d063b6947f056844d3ba38ce85930e267a
Jonggwan Park <30569896+little7279@users.noreply.github.com>
2023-04-21 20:49:54 +0900

    P5Component.vue

--- src/components/P5Component.vue
+++ src/components/P5Component.vue
@@ -4,25 +4,26 @@
 
 <script lang="ts">
 import { defineComponent, ref, onMounted } from 'vue'
-import * as p5 from 'p5'
+import p5 from 'p5'
 
 export default {
   name: 'P5Component',
   setup() {
-    const canvas = ref<HTMLCanvasElement | null>(null)
+    const canvas = ref<HTMLCanvasElement | undefined>(undefined)
+
     onMounted(() => {
-      new p5((sketch) => {
-        sketch.setup = () => {
-          sketch.createCanvas(400, 400)
+      new p5((p: p5) => {
+        p.setup = () => {
+          p.createCanvas(400, 400)
         }
 
-        sketch.draw = () => {
-          sketch.background(0)
-          sketch.fill(255)
-          sketch.rect(100, 100, 200, 200)
+        p.draw = () => {
+          p.background(0)
+          p.fill(255)
+          p.rect(100, 100, 200, 200)
         }
       }, canvas.value)
-    })
+    });
     return { canvas }
   }
 }
